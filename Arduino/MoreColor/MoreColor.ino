80c2505bc3c4ba975913c70c19b3aef24610a1e6
AppaBabba <darleg@outlook.com>
2018-07-01 01:13:14 -0700

    removed what was not needed

--- Arduino/MoreColor/MoreColor.ino
+++ Arduino/MoreColor/MoreColor.ino
@@ -1,44 +1,11 @@
 /*
-  Colorterm -  Demo for Terminals.
+  MoreColor -  Demo for Terminals.
   Created by Darell J LeGare, Junw 26, 2018.
   Released into the public domain.
 */
 
 #include <DispOnTerm.h>
 
-byte myrow;
-byte mycol;
-byte myat;
-byte myfg;
-byte mybg;
-enum myat {
-  rst,
-  bld,
-  dim,
-  so,
-  us,
-  blink
-};
-enum myfg {
-  fgblk  = 30,
-  fgred  = 31,
-  fggrn  = 32,
-  fgylw  = 33,
-  fgblue = 34,
-  fgmgnt = 35,
-  fgcyn  = 36,
-  fgwht  = 37
-};
-enum mybg {
-  bgblk  = 40,
-  bgred  = 41,
-  bggrn  = 42,
-  bgylw  = 43,
-  bgblue = 44,
-  bgmgnt = 45,
-  bgcyn  = 46,
-  bgwht  = 47
-};
 
 DispOnTerm ColorTerm("Appa Software");
 
@@ -47,7 +14,6 @@ void setup()
 {
   Serial.begin(9600);
   ColorTerm.clscn();
-  //ColorTerm.info();
 }
 
 void loop()
