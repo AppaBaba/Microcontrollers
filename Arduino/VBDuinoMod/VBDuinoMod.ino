0f43bcf2945c999cc2b6effb401a25af1f2c718b
AppaBabba <darleg@outlook.com>
2018-07-18 17:29:00 -0700

    update

--- Arduino/VBDuinoMod/VBDuinoMod.ino
+++ Arduino/VBDuinoMod/VBDuinoMod.ino
@@ -1,3 +1,17 @@
+/*
+  VBDuinoMod - Used to send number commands to
+  Arduino. Goes with the Visual Basic project
+  VBDuinoMod. Sends back Staus Also, works
+  with Arduino Serial Monitor.
+  It now works with my new VBDuinoMon program.
+  It is A Visual Basic Arduino Serial Monitor
+  look-alike.
+  
+  
+  Created by Darell J LeGare, July 10, 2018.
+  Released into the public domain.
+*/
+
 void setup() {
   pinMode(12,OUTPUT);
   Serial.begin(9600);
